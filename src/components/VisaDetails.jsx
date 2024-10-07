6b334c50dc8a243356c375e618a281da74030db7
usman ali <usmanali477275@gmail.com>
2024-10-07 12:08:02 +0500

    changes

--- src/components/VisaDetails.jsx
+++ src/components/VisaDetails.jsx
@@ -6,34 +6,44 @@ const VisaDataDisplay = ({ details }) => {
     <div className="grid grid-cols-1 gap-2 mb-6 text-sm">
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">e-Visa Number:</span>
-        <span className=" font-semibold ">{details.visaNumber}</span>
+        <span className=" w-[60%] text-start font-semibold ">
+          {details.visaNumber}
+        </span>
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Name:</span>
-        <span className=" font-semibold uppercase">{details.Name}</span>
+        <span className=" w-[60%] text-start font-semibold ">
+          {details.Name}
+        </span>
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Date of Birth:</span>
-        <span className=" font-semibold">
+        <span className=" w-[60%] text-start font-semibold ">
           {" "}
           {details.Dob ? moment(details.Dob).format("DD-MM-YYYY") : "N/A"}
         </span>
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Sex:</span>
-        <span className=" font-semibold">{details.Sex}</span>
+        <span className=" w-[60%] text-start font-semibold ">
+          {details.Sex}
+        </span>
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Nationality:</span>
-        <span className=" font-semibold">{details.Nationality}</span>
+        <span className=" w-[60%] text-start font-semibold ">
+          {details.Nationality}
+        </span>
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Travel Document Number:</span>
-        <span className=" font-semibold">{details.TravelNumber}</span>
+        <span className=" w-[60%] text-start font-semibold ">
+          {details.TravelNumber}
+        </span>
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Visa Issue Date:</span>
-        <span className=" font-semibold">
+        <span className=" w-[60%] text-start font-semibold ">
           {" "}
           {details.issue_date
             ? moment(details.issue_date).format("DD-MM-YYYY")
@@ -42,7 +52,7 @@ const VisaDataDisplay = ({ details }) => {
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Visa Valid Till:</span>
-        <span className="font-semibold">
+        <span className=" w-[60%] text-start font-semibold ">
           {details.valid_till
             ? moment(details.valid_till).format("DD-MM-YYYY")
             : "N/A"}
@@ -50,29 +60,29 @@ const VisaDataDisplay = ({ details }) => {
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Type of Visa:</span>
-        <span className=" font-semibold">MULTIPLE</span>
+        <span className=" w-[60%] text-start font-semibold ">MULTIPLE</span>
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Remarks:</span>
-        <span className=" font-semibold">
+        <span className=" w-[60%] text-start font-semibold ">
           Not Valid for Employment outside Four point of Sheraton
         </span>
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Visa Issuing Authority:</span>
-        <span className=" font-semibold">
+        <span className=" w-[60%] text-start font-semibold ">
           IMMIGRATION NEW ZEALAND &CHECKPOINT AUTHORITY
         </span>
       </div>
       <div className="flex relative w-full justify-between">
         <span className="font-bold w-1/3">Visa Processing Fee:</span>
-        <span className=" font-semibold">572/ NZD</span>
+        <span className=" w-[60%] text-start font-semibold ">572/ NZD</span>
       </div>
       <div className="w-full flex justify-start mt-4 flex-col gap-2">
         <div className="barcode"></div>
-        <div className="font-semibold w-[60%] text-center">
+        <span className=" w-[60%] text-start font-semibold ">
           {details.visaNumber}
-        </div>
+        </span>
       </div>
     </div>
   );
